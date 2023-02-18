db07e975877d4fa0be8c445f623f0b6079dec0d2
Eisiehame <knurov24@gmail.com>
2023-02-18 19:38:48 +0200

    lesson 13.2_1-1

--- utils.py
+++ utils.py
@@ -1,16 +1,28 @@
+import csv
 
 class Item:
 
-    num_of_items = 0
+    num_of_items = []
     discount_price = 1
 
     def __init__(self, name, price, quantity):
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
 
-        Item.num_of_items += 1
+
+        Item.num_of_items.append(self.__name)
+
+    # @name.setter
+    # def name(self, name : str):
+    #     if len(self.__name) > 10:
+    #         raise Exception('Длина наименования товара превышает 10 символов.')
+    #     else:
+    #         self.__name = name
+
+
+
 
     def calculate_total_price(self):
         return (self.price * self.quantity)
