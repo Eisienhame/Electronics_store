cf377d4a9b3d9ceb2f0f0b082b2e08fe35500cd9
Eisiehame <knurov24@gmail.com>
2023-02-23 13:02:10 +0200

    lesson 14.1 - finish

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,11 @@
 import pytest
+from utils import Item
+@pytest.fixture
+def item_test():
+    x = Item(name='ТОВАР', price=10000, quantity=20)
+    return x
+
+@pytest.fixture
+def item_test2():
+    x = Item(name='ТОВАРТОВАРТОВАР', price=10000, quantity=20)
+    return x
