27e237412ecf4a99336943c1b2bc03f100452387
Eisiehame <knurov24@gmail.com>
2023-03-02 19:12:29 +0200

    lesson 15.1-1 - finish

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from utils import Item
+from utils import Item, Phone
 @pytest.fixture
 def item_test():
     x = Item(name='ТОВАР', price=10000, quantity=20)
@@ -8,4 +8,9 @@ def item_test():
 @pytest.fixture
 def item_test2():
     x = Item(name='ТОВАРТОВАРТОВАР', price=10000, quantity=20)
+    return x
+
+@pytest.fixture
+def item_test3():
+    x = Phone(name='ТЕЛЕФОН', price=10000, quantity=20, number_of_sim = 3)
     return x
--- utils.py
+++ utils.py
@@ -67,23 +67,23 @@ class Item:
 
 class Phone(Item):
     def __init__(self, name, price, quantity, number_of_sim):
-        self.sim_count = number_of_sim
+        self.__number_of_sim = number_of_sim
         super().__init__(name, price, quantity)
 
     def __repr__(self):
-        return f'{self.__class__.__name__}("{self.__name}", {self.price}, {self.quantity}), {self.number_of_sim}'
+        return f'{self.__class__.__name__}("{self.__name}", {self.price}, {self.quantity}), {self.__number_of_sim}'
 
     @property
     def number_of_sim(self, number_of_sim=None):
         if number_of_sim == None:
-            return self.number_of_sim
+            return self.__number_of_sim
         else:
-            self.number_of_sim = number_of_sim
-            return self.number_of_sim
+            self.__number_of_sim = number_of_sim
+            return self.__number_of_sim
 
     @number_of_sim.setter
     def number_of_sim(self, number_of_sim):
-        if number_of_sim < 1:
+        if number_of_sim < 1 or number_of_sim % 1 != 0:
             raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
         else:
-             self.number_of_sim = number_of_sim
+             self.__number_of_sim = number_of_sim
